21987a05c6ed23c92b9b4263024cf91364584648
Dan Villa <danielavilla02@gmail.com>
2022-12-14 07:47:02 -0600

    chore: add global guard and open a few routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    // { provide: APP_GUARD, useClass: LocalAuthGuard }
-  ],
+  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
